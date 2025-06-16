314bebc70cc74d6e0db02136d0e75af1a684eafa
Anshu <kushwaha.anshu8@gmail.com>
2025-06-16 11:26:58 +0530

    weather-app-complete

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-  const city = document.querySelector("#city-input");
+  const cityInput = document.querySelector("#city-input");
 
   const weatherInfo = document.getElementById("weather-info");
 
-  const cityName = document.getElementById("city-name");
+  const cityNameDisplay = document.getElementById("city-name");
 
   const temperature = document.getElementById("temperature");
 
   const description = document.getElementById("description");
 
   const errorMessage = document.querySelector("#error-message");
+
+  //   Hit the API now
+  const API_KEY = "4140ae0bc458d0c765e72b2b25e8e74a";
   document
     .querySelector("#get-weather-btn")
-    .addEventListener("click", function () {
-      if (city.value.length === 0) {
+    .addEventListener("click", async () => {
+      const city = cityInput.value.trim();
+      if (city.length === 0) {
         errorMessage.classList.remove("hidden");
+        weatherInfo.classList.add("hidden");
       } else {
+        weatherInfo.classList.remove("hidden");
         errorMessage.classList.add("hidden");
-      }
+        try {
+          const weatherData = await fetchWeatherData(city);
 
-      //   Hit the API now
+          displayWeatherData(weatherData);
+        } catch (error) {
+          showError();
+        }
+      }
     });
+
+  async function fetchWeatherData(cityName) {
+    const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      throw new error("City not found");
+    }
+    // response.then()
+
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(data) {
+    const { name, main, weather } = data;
+
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+    cityNameDisplay.textContent = name;
+    temperature.textContent = `Temperature : ${main.temp}`;
+    description.textContent = `Weather : ${weather[0].description}`;
+  }
+
+  function showError() {
+    errorMessage.classList.remove("hidden");
+    weatherInfo.classList.add("hidden");
+  }
 });
